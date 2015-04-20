a56295856000455e3be1245a5f3709e34aa92daa
root <root@debian7-template.cloud.mos.meituan.com>
2015-04-20 18:09:29 +0800

    commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php defined( '_JEXEC' ) or die;?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" 
+      xml:lang="<?php echo $this->language; ?>" 
+      lang="<?php echo $this->language; ?>" 
+      dir="<?php echo $this->direction; ?>">
+<head>
+<jdoc:include type="head" />
+</head>
+<body>
+<jdoc:include type="modules" name="position-1" style="none" />   
+<jdoc:include type="modules" name="position-2" style="none" />
+<jdoc:include type="modules" name="position-7" style="none" />
+<jdoc:include type="component" />
+<jdoc:include type="modules" name="footer" style="none" /> 
+</body>
+</html>
